56331bdfa2b2831f8c28414408a074bf95cecf5e
VasylMolnar <vasylmolnar281@gmail.com>
2022-10-27 10:05:20 +0300

    Update

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-import express from 'express'
-const app = express();
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
-app
+import authRoute from './routes/auth.js';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('____________Connected to mongoDB.____________');
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('___________mongoDB disconnected!____________');
+});
+
+//middleware
+app.use('/', authRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log('____________Connected to backend.____________');
+});
