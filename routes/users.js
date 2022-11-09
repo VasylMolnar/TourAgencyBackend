56d396fa7a8b30a3e558721e41e6e78feac327c2
VasylMolnar <vasylmolnar281@gmail.com>
2022-11-09 16:02:57 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.put('/:id', verifyUser, updateUser);
 router.delete('/:id', verifyUser, deleteUser);
 
 //GET
-router.get('/:id', verifyUser, getUser);
+router.get('/:id', getUser);
 
 //GET ALL
 router.get('/', verifyAdmin, getUsers);
